65da0d378e0dda243714e059516c4b6af2407c25
Dixon Wang <dixonwang@Dixons-MacBook-Pro.local>
2018-08-23 14:05:14 +1200

    dixon

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter_go/blog_post_page.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_go/blog_card.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CreateBlogPage extends StatefulWidget {
   @override
@@ -7,20 +12,57 @@ class CreateBlogPage extends StatefulWidget {
 }
 
 class _CreateBlogPageState extends State<CreateBlogPage> {
-  List<dynamic> _blogs;
+  List _blogs;
+
+  Future<dynamic> getStore() async {
+    final prefs = await SharedPreferences.getInstance();
+    final response = await http.get(
+        'https://dev.gogox.co.nz/v1/core/testblog/blogs?userID=' +
+            prefs.getString('uid'));
+    final responseJson = json.decode(response.body);
+    final int statusCode = response.statusCode;
+
+    if (statusCode == 200) {
+      setState(() {
+        _blogs = responseJson['data'];
+      });
+      return null;
+    } else {
+      return 'error';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getStore();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ORDERS'),
+        title: Text('Blogs'),
       ),
       backgroundColor: Colors.grey,
       body: ListView.builder(
         itemCount: _blogs.length,
         itemBuilder: (context, index) {
           return BlogCard(
-            title: _blogs[1],
+            title: _blogs[index]['title'],
+            content: _blogs[index]['content'],
+            user: _blogs[index]['user'],
+            createdAt: _blogs[index]['createdAt'],
+          );
+        },
+      ),
+      floatingActionButton: RaisedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PostBLogPage(),
+            ),
           );
         },
       ),
---
+++ lib/blog_post_page.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PostBLogPage extends StatefulWidget {
+  @override
+  _PostBLogPageState createState() => _PostBLogPageState();
+}
+
+class _PostBLogPageState extends State<PostBLogPage> {
+  final formKey = GlobalKey<FormState>();
+  String _title, _content;
+
+  void _submit() {
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      setState(() {
+        form.save();
+        this.postBlog();
+      });
+    }
+  }
+
+  Future<dynamic> postBlog() async {
+    final prefs = await SharedPreferences.getInstance();
+    final response = await http.post(
+      'https://dev.gogox.co.nz/v1/core/testblog/blog',
+      headers: {'Content-Type': 'application/json'},
+      body: {
+        'title': _title,
+        'content': _content,
+        'userID': prefs.getString('uid'),
+      },
+    );
+    //final responseJson = json.decode(response.body);
+    final int statusCode = response.statusCode;
+
+    if (statusCode == 200) {
+      return Navigator.pop(context);
+    } else {
+      return 'failed';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New BLog'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Title cannot be empty.';
+                  }
+                },
+                onSaved: (val) => _title = val,
+                autofocus: false,
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  prefixIcon: Icon(Icons.account_box),
+                ),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'content cannot be empty.';
+                  }
+                },
+                onSaved: (val) => _content = val,
+                autofocus: false,
+                decoration: InputDecoration(
+                  labelText: 'Content',
+                  prefixIcon: Icon(Icons.vpn_key),
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RaisedButton(
+                child: Text(
+                  'POST',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  _submit();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
